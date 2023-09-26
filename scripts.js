52723f7fd5e1846508b515a60968959dfb491c0a
Davi Sollar <davisoca10@gmail.com>
2023-09-26 08:42:35 -0300

    bah

--- scripts.js
+++ scripts.js
@@ -1,10 +1,25 @@
+window.onscroll = function () {
+  stickynavbar()
+};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function stickynavbar() {
+  if (window.screenY || document.documentElement.scrollTop >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
 function openModal(event) {
   let modal = document.getElementById("myModal");
   let images = document.getElementsByClassName("images");
   let modalImg = document.getElementById("img01");
   for (let i = 0; i < images.length; i++) {
     let img = images[i];
-    img.onclick = function(evt) {
+    img.onclick = function (evt) {
       modal.style.display = "block";
       modalImg.src = this.src;
     }
